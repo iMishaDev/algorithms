fd51a5e3798496d46b4e7b1c73cc85467ebb5e9b
Mashael <m.alsaeedi@safcsp.org.sa>
2021-06-09 10:08:08 +0300

    enhance insertion sort

--- InsertionSort.js
+++ InsertionSort.js
@@ -4,18 +4,11 @@ class InsertionSort {
         for(let i = 1; i < arr.length; i++){
             for(let j = i; j > -1; j--){
                 if(arr[j] < arr[j-1]){
-                    [arr[j], arr[j-1]] = this.#swap(arr[j], arr[j-1])
+                    [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
                 }
             }
         }
         return arr
-    }
-
-    static #swap(element1, element2){
-        let temp = element1
-        element1 = element2
-        element2 = temp
-        return [ element1, element2 ]
     }
 }
 
