735d74dbf3c4b6f47e961e957f3a52ab828ebdc0
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-15 14:11:12 +0300

    WIP fixing quickSort

--- QuickSort.js
+++ QuickSort.js
@@ -5,29 +5,51 @@ class QuickSort {
         return arr
     }
 
-    static #sortHelper(arr, start, end){
-        if(end <= start) return; 
-        let mid = Math.floor((start + end)/2)
-        this.#sortHelper(arr, start, mid)
-        this.#sortHelper(arr, mid + 1, end)
-        this.#sorter(arr, start, end, mid)
+    static #partitioner(arr, start, end) {
+        const pivotValue = arr[end];
+        let pivotIndex = start;
+
+        while(start < end){
+            if(arr[start] < pivotValue){
+                [arr[pivotIndex], arr[start]] = [arr[start], arr[pivotIndex]];
+                pivotIndex ++;
+            }
+            start ++;
+        }
+        [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+
+        return pivotIndex;
     }
 
-    static #sorter(arr, start, end, pivot){
-        while (start <= end){
-            if(arr[start] >= arr[pivot] && arr[end] >= arr[pivot]){
-                end--;
-            }
-            if(arr[end] <= arr[pivot] && arr[start] <= arr[pivot]){
-                start++;
-            }
-            if(arr[start] > arr[pivot] && arr[end] < arr[pivot]){
-                [arr[start], arr[end]] = [arr[end], arr[start]]
-                start ++;
-                end --;
-            }
-        }
+
+    static #sortHelper(arr, start, end){
+        if(end <= start ) return; 
+        let i = this.#partitioner(arr, start, end);
+        console.log(i)
+        this.#sortHelper(arr, start, i - 1)
+        this.#sortHelper(arr, i + 1, end)
     }
+
+    // static #sorter(arr, start, end, pivot){
+    //     console.log(arr.slice(start, end+1), pivot)
+
+    //     while (start <= end){
+            
+    //         if(arr[end] > arr[pivot]){
+    //             end--;
+    //         }
+    //         if(arr[start] < arr[pivot]){
+    //             start++;
+    //         }
+    //         if(arr[start] >= arr[pivot] && arr[end] <= arr[pivot]){
+    //             [arr[start], arr[end]] = [arr[end], arr[start]]
+    //             start ++;
+    //             end --;
+    //         }
+            
+    //     }
+
+    // }
 }
 
 console.log('Original :', [10,5,7,33,6,1])
