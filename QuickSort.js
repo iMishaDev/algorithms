78da51d904f493a151093f560bebd14d046a5dc7
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-15 14:27:17 +0300

    quicksort cleanup

--- QuickSort.js
+++ QuickSort.js
@@ -24,33 +24,13 @@ class QuickSort {
 
     static #sortHelper(arr, start, end){
         if(end <= start ) return; 
-        let i = this.#partitioner(arr, start, end);
-        console.log(i)
-        this.#sortHelper(arr, start, i - 1)
-        this.#sortHelper(arr, i + 1, end)
+        let pivotIndex = this.#partitioner(arr, start, end);
+
+        this.#sortHelper(arr, start, pivotIndex - 1)
+        this.#sortHelper(arr, pivotIndex + 1, end)
     }
 
-    // static #sorter(arr, start, end, pivot){
-    //     console.log(arr.slice(start, end+1), pivot)
-
-    //     while (start <= end){
-            
-    //         if(arr[end] > arr[pivot]){
-    //             end--;
-    //         }
-    //         if(arr[start] < arr[pivot]){
-    //             start++;
-    //         }
-    //         if(arr[start] >= arr[pivot] && arr[end] <= arr[pivot]){
-    //             [arr[start], arr[end]] = [arr[end], arr[start]]
-    //             start ++;
-    //             end --;
-    //         }
-            
-    //     }
-
-    // }
 }
 
-console.log('Original :', [10,5,7,33,6,1])
-console.log('Sorted', QuickSort.sort([10,5,7,33,6,1]))
+console.log('Original :', [33,5,7,1,6,10])
+console.log('Sorted', QuickSort.sort([33,5,7,1,6,10]))
