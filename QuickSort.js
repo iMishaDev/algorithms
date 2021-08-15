d2888d8dfca11effca608974454e8f6c4b37e69b
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-15 16:32:14 +0300

    implement quicksort iterator

--- QuickSort.js
+++ QuickSort.js
@@ -1,8 +1,34 @@
 class QuickSort {
 
-    static sort(arr){
+    static sortRecursive(arr){
         this.#sortHelper(arr, 0, arr.length - 1)
         return arr
+    }
+
+    static sortIterator(arr){
+        let stack = [];
+
+        stack.push(0);
+        stack.push(arr.length-1);
+        while(stack[stack.length -1 ] >= 0){
+            const end = stack.pop();
+            const start = stack.pop();
+
+
+            let pivotIndex = this.#partitioner(arr, start, end);
+            
+            if(pivotIndex - 1  >  start){
+                stack.push(start);
+                stack.push(pivotIndex - 1);
+            }
+
+            if(pivotIndex + 1  <  end){
+                stack.push(pivotIndex + 1);
+                stack.push(end);
+            }
+
+        }
+        return arr;
     }
 
     static #partitioner(arr, start, end) {
@@ -33,4 +59,4 @@ class QuickSort {
 }
 
 console.log('Original :', [33,5,7,1,6,10])
-console.log('Sorted', QuickSort.sort([33,5,7,1,6,10]))
+console.log('Sorted', QuickSort.sortIterator([33,5,7,1,6,10]))
