e0dd43a212c15d6869f54486ab0f5a637436a09c
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-16 14:32:31 +0300

    bucket sort on integers with two digits

--- BucketSort.js
+++ BucketSort.js
@@ -1,6 +1,6 @@
 class BucketSort {
 
-    sort(arr){
+    sortDecimals(arr){
         let buckets = new Map()
         let index = 0;
         let sorted = [];
@@ -24,7 +24,36 @@ class BucketSort {
 
         return sorted;
     }
+
+    sortIntegers(arr){
+        let buckets = new Map()
+        let index = 0;
+        let sorted = [];
+
+        for(let i = 0; i < 10; i++){
+            buckets.set(i, [])
+        }
+        
+        for(let i = 0; i < arr.length; i++){
+            index = Math.floor(arr[i] / 10);
+            buckets.get(index).push(arr[i])
+        }
+
+        for(let i = 0; i < buckets.size; i++){
+            buckets.get(index).sort()
+        }
+        
+        for(let i = 0; i < buckets.size; i++){
+            sorted = [...sorted, ...buckets.get(i)]
+        }
+
+        return sorted;
+    }
 }
 
 console.log('Original :', [.42, .32, .33, .52, .37, .47, .51])
-console.log('Sorted', new BucketSort().sort([.42, .32, .33, .52, .37, .47, .51]))
+console.log('Sorted', new BucketSort().sortDecimals([.42, .32, .33, .52, .37, .47, .51]))
+
+
+console.log('Original :', [ 15, 1, 32, 10, 80, 2, 12, 90, 99, 11])
+console.log('Sorted', new BucketSort().sortIntegers([  15, 1, 32, 10, 80, 2, 12, 90, 99, 11]))
