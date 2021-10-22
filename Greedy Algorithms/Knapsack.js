b426d7039460ee0e06a79bab3aeef20264da2bee
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-22 12:40:49 +0300

    implement 0/1 Lnapsack algorithm

--- Greedy Algorithms/Knapsack.js
+++ Greedy Algorithms/Knapsack.js
@@ -42,6 +42,33 @@ class Solution {
         }
         return  totalProfit
     }
+
+    get01KNapsack(wt, p, max){
+        let table  = new Array(wt.length).fill(0).map(() => new Array(max + 1).fill(0))
+        let result = [];
+
+        for(let i = 0; i < table.length; i++){
+            for(let w = 0; w < table[i].length; w++){
+                if(i == 0 || w == 0)
+                    table[i][w] = 0;
+                else if(wt[i] <= w){
+                    table[i][w] = Math.max(p[i] + table[i - 1][w - wt[i]], table[i - 1][w])
+                } else table[i][w] = table[i - 1][w];
+            }
+        }
+
+        let w = table.length - 1;
+        let i = table[0].length - 1;
+        while( w > 0 && i > 0){
+            if(table[w][i] !== table[w - 1][i]){
+                result.push(w);
+                i -= wt[w]
+            }
+            w -= 1;
+        }
+
+        console.log(result)
+    }
 }
 let objects = [
     new CObject(4, 18),
@@ -52,4 +79,8 @@ let objects = [
     new CObject(1, 6),
     new CObject(1, 3),
 ]
+
+let wt = [0, 2, 3, 4, 5];
+let p = [0, 1, 2, 5, 6];
 console.log(new Solution().get(15, objects))
+console.log(new Solution().get01KNapsack(wt, p, 8))
