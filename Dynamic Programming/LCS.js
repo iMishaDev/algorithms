6b76d9ac595d90faedd7d409e1d8c3c53b102cae
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-28 20:25:02 +0300

    use dynamic programming to solve LCS

--- Dynamic Programming/LCS.js
+++ Dynamic Programming/LCS.js
@@ -2,6 +2,8 @@ class LongestCommonSubsequence {
     constructor(word, sequence){
         this.word = word;
         this.sequence = sequence;
+        this.matrix = new Array(this.word.length +1 ).fill(0).map((el) => new Array(this.sequence.length + 1).fill(0));
+
     }
 
     get_longest(){
@@ -15,7 +17,22 @@ class LongestCommonSubsequence {
             return 1 + this.get_longest_helper(i + 1, j + 1);
         else return Math.max(this.get_longest_helper(i + 1, j), this.get_longest_helper(i, j + 1))
     }
+
+    get_longest_dynamic(){
+        for(let i = 1; i <= this.word.length; i++){
+            for(let j = 1; j <= this.sequence.length; j++){
+                if(this.word[i - 1] === this.sequence[j - 1])
+                    this.matrix[i][j] = 1 +  this.matrix[i-1][j-1]
+                else this.matrix[i][j] =  Math.max(this.matrix[i - 1][j],this.matrix[i ][j - 1] )
+            }
+        }
+
+        return this.matrix[this.word.length][this.sequence.length];
+    }
 }
 
+let lcs = new LongestCommonSubsequence('bd', 'abcd');
 
-console.log(new LongestCommonSubsequence('abdace', 'babce').get_longest())
+console.log(lcs.get_longest())
+
+console.log(lcs.get_longest_dynamic(1, 1))
